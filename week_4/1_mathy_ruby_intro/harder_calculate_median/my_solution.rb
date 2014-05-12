f73de9a566d65ad884e7d4b15b1845d954c2f5fc
Alex White <awhit012@gmail.com>
2014-05-12 16:44:55 -0700

    median solution'

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -8,18 +8,39 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# # What is the input?
+# input is an array
+# # What is the output? (i.e. What should the code return?)
+# output is item in array that is the middle value
+# # What are the steps needed to solve the problem?
+# sort the array then take length/2
 
 
 # 2. Initial Solution
-
+# def median(array)
+# 	array.sort! #sort array
+# 	if array.length % 2 != 0  #if array is odd
+# 		return array[array.length/2] #return middle item of array
+# 	else  #if even
+# 		return (array[array.length/2] + array[(array.length/2) -1]) /2.0 #average of the two middle items
+# 	end
+# end
 
 
 
 # 3. Refactored Solution
-
+def median(array)
+	array.sort! #sort array
+	len = array.length
+	if len % 2 != 0  #if array is odd
+		return array[len/2] #return middle item of array
+	else  #if even
+		return (array[len/2] + array[(len/2) -1]) /2.0 #average of the two middle items
+	end
+end
 
 
 # 4. Reflection 
+
+# Followed the normal workflow. I got confused byy all of the brackets and parens so the refactored one
+# adds another variable for clarity. 
