d5316637aa1bda682cceca306bdc205ac7dfa42e
Alex White <awhit012@gmail.com>
2014-05-12 14:39:32 -0700

    Add pseudocode to solution

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,14 +4,26 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
+#
+# Add_it_up:
 
+# take array of numbers
+# # iterate through the array 
+# #   add each number
+# # return the sum
+
+# Sentence_maker
+# take an array of strings
+#   concatonate strings together with spaces 
+#   capitalize the first letter of the first string
+#   put a period at the end. 
 
 # 2. Initial Solution
 
