122a09ed39cd6e8b55df2fd4c1ca1c81e8cf7687
Alex White <awhit012@gmail.com>
2014-05-20 15:00:44 -0700

    solo complete

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
@@ -14,10 +14,23 @@
 
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer
   end
   
-  # Make sure you define the other required methods, too
+  def guess(guess)
+  	@guess = guess
+  	if @guess == @answer
+  		:correct
+  	elsif @guess < @answer
+  		:low
+  	else 
+  		:high
+  	end
+  end
+
+  def solved?
+    @guess == @answer
+  end 
 end
 
 
@@ -31,8 +44,21 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+game = GuessingGame.new(10)
 
+p game.solved?   #=> false
 
+p game.guess(5)  # => :low
+p game.guess(20) # => :high
+p game.solved?   # => false
+
+p game.guess(10) # => :correct
+p game.solved?   # => true
+
+# 5. Reflection
+#This was good for more practice with classes methods and variables. Still 
+#shaky onwriting own tests. Not sure where they go. But I'm getting
+#good at writing from tests :)
 
 
 
