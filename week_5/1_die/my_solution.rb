496f5e57f6e952cf75161513cc6d356a5206951b
Alex White <awhit012@gmail.com>
2014-05-20 13:57:22 -0700

    die 1 solution

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,45 +1,60 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge myself
 
-# 2. Pseudocode
+# 2. Pseudocode.
+#Input: putting in a number of sides
+# Output: Getting a random number 
+# Steps: create a die class, initialize sides, raise argument error,
+# get sides, and then generate random number after setting delimiters.
 
-# Input:
-# Output:
-# Steps:
 
 
-# 3. Initial Solution
+
+#3. Initial Solution
+
+# class Die
+
+#   def initialize(sides)
+#   	if sides < 2
+#       raise ArgumentError, "Sides cannot be less than 2" 
+#     end
+#     @sides = sides
+#   end
+  
+#   def sides
+#     return @sides
+#   end
+  
+#   def roll
+#     return (1..sides).to_a.sample
+#   end
+# end
+
+#4. Refactored Solution
 
 class Die
+
   def initialize(sides)
-    # code goes here
+  	if sides < 2
+      raise ArgumentError, "Sides cannot be less than 3" 
+    end
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    return @sides
   end
   
   def roll
-    # code goes here
+    return (1..sides).to_a.sample
   end
 end
+die = Die.new(100)
+puts die.sides 
+puts die.roll
 
-
-
-# 4. Refactored Solution
-
-
-
-
-
-
-# 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
-
-
-# 5. Reflection 
+#Reflection
+#Once I get used to classes and variables in classes this will be a snap! Initial
+#the mean time it is a challenge. Looking forward to more practice. 
